098f24a98e7a5e569fbef46869dc4d6adaf049de
James Gore <73591234+jgore077@users.noreply.github.com>
2024-11-24 09:56:22 -0500

    disable shuffling for reproducibility

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ if __name__=="__main__":
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                             download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE,
-                                            shuffle=True, num_workers=2)
+                                            shuffle=False, num_workers=2)
 
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                         download=True, transform=transform)
